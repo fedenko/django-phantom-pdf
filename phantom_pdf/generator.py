860f2de5fc6b974216abb7186e6718e546b4b95a
Hedde van der Heide <me@heddevanderheide.nl>
2016-05-24 15:45:07 +0200

    Update generator.py

--- phantom_pdf/generator.py
+++ phantom_pdf/generator.py
@@ -84,7 +84,7 @@ class RequestToPDF(object):
         domain = netloc
         return '{protocol}://{domain}{path}'.format(
             protocol=protocol,
-            domain=getattr(settings, 'PHANTOMJS', domain),
+            domain=getattr(settings, 'PHANTOMJS_DOMAIN', domain),
             path=path)
 
     def _save_cookie_data(self, request):
