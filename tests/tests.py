6264fb616c1f7b76b4d6a89df0f52af018cef72c
Constantine Fedenko <cfedenko@gmail.com>
2016-09-21 22:28:03 +0300

    merged node_modules

--- tests/tests.py
+++ tests/tests.py
@@ -9,7 +9,7 @@ class TestPhantomPDF(TestCase):
     @override_settings(
         PHANTOMJS_GENERATE_PDF=os.path.join(BASE_DIR, "generate_pdf.js"),
         PHANTOMJS_PDF_DIR="/tmp/phantom_pdf_test/pdfs",
-        PHANTOMJS_BIN=os.path.join(BASE_DIR, "node_modules", "phantomjs-prebuilt", "bin", "phantomjs"),
+        PHANTOMJS_BIN=os.path.join(os.path.dirname(BASE_DIR), "node_modules", "phantomjs-prebuilt", "bin", "phantomjs"),
         PHANTOMJS_ACCEPT_LANGUAGE="en, uk",
         PHANTOMJS_ORIENTATION="landscape",
         PHANTOMJS_MARGIN="1cm",
@@ -20,7 +20,7 @@ class TestPhantomPDF(TestCase):
         request_to_pdf = phantom_pdf.RequestToPDF()
         self.assertEqual(request_to_pdf.PHANTOMJS_GENERATE_PDF, os.path.join(BASE_DIR, "generate_pdf.js"))
         self.assertEqual(request_to_pdf.PHANTOMJS_PDF_DIR, "/tmp/phantom_pdf_test/pdfs")
-        self.assertEqual(request_to_pdf.PHANTOMJS_BIN, os.path.join(BASE_DIR, "node_modules", "phantomjs-prebuilt", "bin", "phantomjs"))
+        self.assertEqual(request_to_pdf.PHANTOMJS_BIN, os.path.join(os.path.dirname(BASE_DIR), "node_modules", "phantomjs-prebuilt", "bin", "phantomjs"))
         self.assertEqual(request_to_pdf.PHANTOMJS_ACCEPT_LANGUAGE, "en, uk")
         self.assertEqual(request_to_pdf.PHANTOMJS_ORIENTATION, "landscape")
         self.assertEqual(request_to_pdf.PHANTOMJS_MARGIN, "1cm")
@@ -31,7 +31,7 @@ class TestPhantomPDF(TestCase):
     @override_settings(
         PHANTOMJS_GENERATE_PDF=os.path.join(BASE_DIR, "generate_pdf.js"),
         PHANTOMJS_PDF_DIR="/tmp/phantom_pdf_test/pdfs",
-        PHANTOMJS_BIN=os.path.join(BASE_DIR, "node_modules", "phantomjs-prebuilt", "bin", "phantomjs"),
+        PHANTOMJS_BIN=os.path.join(os.path.dirname(BASE_DIR), "node_modules", "phantomjs-prebuilt", "bin", "phantomjs"),
         PHANTOMJS_ACCEPT_LANGUAGE="en, uk",
         PHANTOMJS_PAPER_SIZE={
             "format": "A4",
@@ -48,7 +48,7 @@ class TestPhantomPDF(TestCase):
         request_to_pdf = phantom_pdf.RequestToPDF()
         self.assertEqual(request_to_pdf.PHANTOMJS_GENERATE_PDF, os.path.join(BASE_DIR, "generate_pdf.js"))
         self.assertEqual(request_to_pdf.PHANTOMJS_PDF_DIR, "/tmp/phantom_pdf_test/pdfs")
-        self.assertEqual(request_to_pdf.PHANTOMJS_BIN, os.path.join(BASE_DIR, "node_modules", "phantomjs-prebuilt", "bin", "phantomjs"))
+        self.assertEqual(request_to_pdf.PHANTOMJS_BIN, os.path.join(os.path.dirname(BASE_DIR), "node_modules", "phantomjs-prebuilt", "bin", "phantomjs"))
         self.assertEqual(request_to_pdf.PHANTOMJS_ACCEPT_LANGUAGE, "en, uk")
         self.assertEqual(request_to_pdf.PHANTOMJS_PAPER_SIZE, {
             "format": "A4",
