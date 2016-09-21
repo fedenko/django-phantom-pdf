648d5b5dc4db2b807fdd54d4687904741c1b77c7
Constantine Fedenko <cfedenko@gmail.com>
2016-09-21 23:07:56 +0300

    added live server test

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,28 +1,92 @@
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'fake-key'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sites',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
+
+ROOT_URLCONF = 'admin.site.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'tests.wsgi.application'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db-test.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Password validation
+# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.9/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
 
 LANGUAGES = (
     ('de', 'German'),
     ('en', 'English'),
     ('fr', 'French'),
 )
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = '/static/'
+
+SITE_ID = 1
--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,11 @@
 import os
-from django.test import TestCase, override_settings
+from django.test import TestCase, LiveServerTestCase, override_settings
+from selenium.webdriver.phantomjs.webdriver import WebDriver
 import phantom_pdf
 
 BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
 
-class TestPhantomPDF(TestCase):
+class TestPhantomPDFSettings(TestCase):
 
     @override_settings(
         PHANTOMJS_GENERATE_PDF=os.path.join(BASE_DIR, "generate_pdf.js"),
@@ -60,3 +61,36 @@ class TestPhantomPDF(TestCase):
             }
         })
         self.assertEqual(request_to_pdf.KEEP_PDF_FILES, True)
+
+class TestPhantomPDF(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestPhantomPDF, cls).setUpClass()
+        cls.selenium = WebDriver(executable_path=os.path.join(os.path.dirname(BASE_DIR), "node_modules", "phantomjs-prebuilt", "bin", "phantomjs"))
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(TestPhantomPDF, cls).tearDownClass()
+
+
+    @override_settings(
+        PHANTOMJS_GENERATE_PDF=os.path.join(BASE_DIR, "generate_pdf.js"),
+        PHANTOMJS_PDF_DIR="/tmp/phantom_pdf_test/pdfs",
+        PHANTOMJS_BIN=os.path.join(os.path.dirname(BASE_DIR), "node_modules", "phantomjs-prebuilt", "bin", "phantomjs"),
+        PHANTOMJS_ACCEPT_LANGUAGE="en, uk",
+        PHANTOMJS_PAPER_SIZE={
+            "format": "A4",
+            "orientation": "portrait",
+            "margin": "1.5cm",
+            "footer":{
+                "height": "1cm",
+                "contents": "<p>Footer <span style='float:right'>{{page_num}}/{{num_pages}}</span></p>"
+            }
+        },
+        KEEP_PDF_FILES=True
+    )
+    def test_something(self):
+        request_to_pdf = phantom_pdf.RequestToPDF()
